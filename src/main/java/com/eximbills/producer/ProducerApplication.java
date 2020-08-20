55b68de1d3d20404c312efd9198dad8479fffea2
dayub@chinasystems.com <dayu@DELL-Precision3630.localdomain>
2020-08-20 14:09:59 +0800

    build Docker image

--- src/main/java/com/eximbills/producer/ProducerApplication.java
+++ src/main/java/com/eximbills/producer/ProducerApplication.java
@@ -1,13 +1,31 @@
 package com.eximbills.producer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ProducerApplication {
+
+	@Value("${topic:knative-demo-topic}")
+	private String topic;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProducerApplication.class, args);
+	}
+
+	@Autowired
+	private KafkaTemplate<String, String> template;
+
+	@RequestMapping("/send/{msg}")
+	public void sendMsg(@PathVariable String msg) {
+		this.template.send(topic, msg);
 	}
 
 }
