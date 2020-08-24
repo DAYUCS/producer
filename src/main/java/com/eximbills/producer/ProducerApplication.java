4dab35a23b66e3710153268b21511a9fc84d7a5c
dayub@chinasystems.com <dayu@DELL-Precision3630.localdomain>
2020-08-24 10:39:40 +0800

    change message format to json

--- src/main/java/com/eximbills/producer/ProducerApplication.java
+++ src/main/java/com/eximbills/producer/ProducerApplication.java
@@ -1,5 +1,9 @@
 package com.eximbills.producer;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -13,6 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProducerApplication {
 
+	private final SimpleDateFormat SDF = new SimpleDateFormat("HH:mm:ss");
+
+	private static final String HOSTNAME = System.getenv().getOrDefault("HOSTNAME", "unknow").replace("\n", " - ")
+			.trim();
+
 	@Value("${TOPIC:knative-demo-topic}")
 	private String topic;
 
@@ -25,7 +34,9 @@ public class ProducerApplication {
 
 	@RequestMapping("/send/{msg}")
 	public void sendMsg(@PathVariable String msg) {
-		this.template.send(topic, msg);
+		JSONObject request = new JSONObject().put("message", msg).put("sender", HOSTNAME).put("send-time",
+				SDF.format(new Date()));
+		this.template.send(topic, request.toString());
 	}
 
 }
